c546258dbae8163ee78514afa6093d622747f9e0
Yuka Namboku <29055086+nanbokku@users.noreply.github.com>
2019-06-17 14:04:21 +0900

    [change] Write shader scripts in 'index.html'
    And use webgl-debug.js

--- section2/js/index.js
+++ section2/js/index.js
@@ -3,16 +3,16 @@ window.onload = () => {
 };
 
 let startup = () => {
-  const canvas = document.getElementById('myGLCanvas');
-  const gl = createGLContext(canvas);
+  const canvas = document.getElementById("myGLCanvas");
+  const gl = WebGLDebugUtils.makeDebugContext(createGLContext(canvas));
   const shaderProgram = setupShaders(gl);
   const vertexBuffer = setupBuffers(gl);
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearColor(0.0, 1.0, 0.0, 1.0);
   draw(gl, shaderProgram, vertexBuffer);
 };
 
 let createGLContext = canvas => {
-  const names = ['webgl', 'experimental-webgl'];
+  const names = ["webgl", "experimental-webgl"];
   let context = null;
 
   for (var i = 0; i < names.length; i++) {
@@ -30,13 +30,41 @@ let createGLContext = canvas => {
     if (!window.WebGLRenderingContext) {
       // このブラウザはWebGLを認識しない
       // ユーザーがブラウザの更新情報を得られるように"http://get.webgl.org"へのリンクを示す．
-      alert('Access to http://get.webgl.org');
+      alert("Access to http://get.webgl.org");
     } else {
-      alert('Access to http://get.webgl.org/troubleshooting');
+      alert("Access to http://get.webgl.org/troubleshooting");
     }
   }
 
   return context;
+};
+
+let loadShaderFromDOM = (gl, id) => {
+  const shaderScript = document.getElementById(id);
+  if (!shaderScript) {
+    return null;
+  }
+
+  const shaderSource = shaderScript.textContent;
+  let shader;
+  if (shaderScript.type === "x-shader/x-fragment") {
+    shader = gl.createShader(gl.FRAGMENT_SHADER);
+  } else if (shaderScript.type === "x-shader/x-vertex") {
+    shader = gl.createShader(gl.VERTEX_SHADER);
+  } else {
+    return null;
+  }
+
+  gl.shaderSource(shader, shaderSource);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert(gl.getShaderInfoLog(shader));
+    deleteShader(shader);
+    return null;
+  }
+
+  return shader;
 };
 
 let loadShader = (gl, type, shaderSource) => {
@@ -45,7 +73,7 @@ let loadShader = (gl, type, shaderSource) => {
   gl.compileShader(shader);
 
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert('Error compiling shader' + gl.getShaderInfoLog(shader));
+    alert("Error compiling shader" + gl.getShaderInfoLog(shader));
     gl.deleteShader(shader);
     return null;
   }
@@ -54,23 +82,26 @@ let loadShader = (gl, type, shaderSource) => {
 };
 
 let setupShaders = gl => {
-  const vertexShaderSource =
-    'attribute vec3 aVertexPosition;\n' +
-    'void main() {\n' +
-    ' gl_Position = vec4(aVertexPosition, 1.0);\n' +
-    '}\n';
-  const fragmentShaderSource =
-    'precision mediump float;\n' +
-    'void main() {\n' +
-    ' gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n' +
-    '}\n';
+  // const vertexShaderSource =
+  //   "attribute vec3 aVertexPosition;\n" +
+  //   "void main() {\n" +
+  //   " gl_Position = vec4(aVertexPosition, 1.0);\n" +
+  //   "}\n";
+  // const fragmentShaderSource =
+  //   "precision mediump float;\n" +
+  //   "void main() {\n" +
+  //   " gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n" +
+  //   "}\n";
 
-  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-  const fragmentShader = loadShader(
-    gl,
-    gl.FRAGMENT_SHADER,
-    fragmentShaderSource
-  );
+  // const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  // const fragmentShader = loadShader(
+  //   gl,
+  //   gl.FRAGMENT_SHADER,
+  //   fragmentShaderSource
+  // );
+
+  const vertexShader = loadShaderFromDOM(gl, "shader-vs");
+  const fragmentShader = loadShaderFromDOM(gl, "shader-fs");
 
   const shaderProgram = gl.createProgram();
   gl.attachShader(shaderProgram, vertexShader);
@@ -78,14 +109,14 @@ let setupShaders = gl => {
   gl.linkProgram(shaderProgram);
 
   if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    alert('Failed to setup shaders');
+    alert("Failed to setup shaders");
   }
 
   gl.useProgram(shaderProgram);
 
   shaderProgram.vertexPositionAttribute = gl.getAttribLocation(
     shaderProgram,
-    'aVertexPosition'
+    "aVertexPosition"
   );
 
   return shaderProgram;
